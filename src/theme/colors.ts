415ae62898453fdfd007f2e329daea10133e6eae
polyfrogfi <83506329+polyfrogfi@users.noreply.github.com>
2021-05-01 15:02:50 -0400

    Add files via upload

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,10 +2,10 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#d9593c",
-  primaryBright: "#d9593c",
-  primaryDark: "#d9593c",
-  secondary: "#da583e",
+  primary: "#3cd93f",
+  primaryBright: "#3cd93f",
+  primaryDark: "#3cd93f",
+  secondary: "#3ac23d",
   success: "#31D0AA",
   warning: "#FFB237",
 };
