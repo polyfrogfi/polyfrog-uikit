f22e366326e97f1e0526d551c0fb18d380f94d54
polyfrogfi <83506329+polyfrogfi@users.noreply.github.com>
2021-05-02 12:20:21 -0400

    Add files via upload

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2843,9 +2843,9 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#3cd93f",
-    primaryBright: "#3cd93f",
-    primaryDark: "#3cd93f",
+    primary: "#268926",
+    primaryBright: "#268926",
+    primaryDark: "#268926",
     secondary: "#3ac23d",
     success: "#31D0AA",
     warning: "#FFB237",
@@ -2853,7 +2853,7 @@ var baseColors = {
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#da583e", textDisabled: "#BDC2C4", textSubtle: "#da583e", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#268926", textDisabled: "#BDC2C4", textSubtle: "#268926", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
 var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#f09746", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#f09746", textDisabled: "#666171", textSubtle: "#f09746", borderColor: "#524B63", card: "#27262c", gradients: {
